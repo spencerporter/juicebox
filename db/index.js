a6b632cd03abbc7fd70a417e47ef23d3aa4d60a1
spencerporter <scfp419@gmail.com>
2021-10-19 19:32:17 -0600

    Ignore Node Modules & duplicate name fix

--- db/index.js
+++ db/index.js
@@ -17,7 +17,9 @@ async function createUser({ username, password }) {
     try {
         const result = await client.query(`
         INSERT INTO users(username, password)
-        VALUES ($1, $2);
+        VALUES ($1, $2)
+        ON CONFLICT (username) DO NOTHING 
+        RETURNING *;
         `, [username, password]);
 
         return result;
