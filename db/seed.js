9503b512fb3d22ecd766c3351577087466d95ef1
spencerporter <scfp419@gmail.com>
2021-10-23 11:05:12 -0600

    Remove unused function

--- db/seed.js
+++ db/seed.js
@@ -126,30 +126,6 @@ async function createInitialPosts(){
     }
 }
 
-// async function createInitialTags(){
-//     try {
-//         console.log("Starting to create tags...");
-
-//         const [happy, sad, inspo, catman] = await createTags([
-//           '#happy', 
-//           '#worst-day-ever', 
-//           '#youcandoanything',
-//           '#catmandoeverything'
-//         ]);
-    
-//         const [postOne, postTwo, postThree] = await getAllPosts();
-    
-//         await addTagsToPost(postOne.id, [happy, inspo]);
-//         await addTagsToPost(postTwo.id, [sad, inspo]);
-//         await addTagsToPost(postThree.id, [happy, catman, inspo]);
-    
-//         console.log("Finished creating tags!");
-//     } catch (error) {
-//         console.error("Error creating tags");
-//         throw error;
-//     }
-// }
-  
 async function testDB() {
     try {
         console.log("Starting to test database...");
