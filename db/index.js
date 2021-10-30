0bc5afc850dbfb379af7731775f63703da5fb71c
spencerporter <scfp419@gmail.com>
2021-10-30 10:58:55 -0600

    database heroku config

--- db/index.js
+++ db/index.js
@@ -2,7 +2,7 @@
 const { Client } = require('pg'); // imports the pg module
 
 // supply the db name and location of the database
-const client = new Client('postgres://localhost:5432/juicebox-dev');
+const client = new Client(process.env.DATABASE_URL || 'postgres://localhost:5432/juicebox-dev');
 
 async function getAllUsers() {
     const { rows } = await client.query(
